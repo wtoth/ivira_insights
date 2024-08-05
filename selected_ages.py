1a666b8aa0f172beca4d4c3871d49874cb0f63ae
wtoth <wtoth@umich.edu>
2024-08-05 14:54:30 -0400

    Moved uploader & updated Age Group Selection to slider

--- selected_ages.py
+++ selected_ages.py
@@ -4,13 +4,3 @@ def calculate_age(born):
     born = datetime.datetime.strptime(born, "%m/%d/%Y")
     today = datetime.date.today()
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
-def select_ages(ages):
-    output = []
-    counter = 1
-    for age in ages:
-        if age:
-            for num in range(counter, counter + 10):
-                output.append(num)
-        counter += 10
-    return output
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,47 +2,36 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from selected_ages import select_ages, calculate_age
+from selected_ages import calculate_age
 
 st.title("Ivira Insights Report")
 
-file_upload = st.file_uploader("upload file", type={"xlsx", "csv"})
-if file_upload is not None:
-    suffix = Path(file_upload.name).suffix
-    if suffix == ".xlsx":
-            df = pd.read_excel(file_upload)
-    elif suffix == ".csv":
-            df = pd.read_csv(file_upload)
-    else:
-        st.write("upload file type failed")
+with st.sidebar:
+    file_upload = st.file_uploader("upload file", type={"xlsx", "csv"})
+    if file_upload is not None:
+        suffix = Path(file_upload.name).suffix
+        if suffix == ".xlsx":
+                df = pd.read_excel(file_upload)
+        elif suffix == ".csv":
+                df = pd.read_csv(file_upload)
+        else:
+            st.write("upload file type failed")
 
-    df.dropna(subset="Patient Id", inplace=True)
-
-    insurance_options = list(df["Primary Plan Name"].unique())
-
-    with st.sidebar:
+        df.dropna(subset="Patient Id", inplace=True)
+        insurance_options = list(df["Primary Plan Name"].unique())
         with st.expander("Select Age Groups"):
-            age_0_10 = st.checkbox("1-10", value=True)
-            age_11_20 = st.checkbox("11-20", value=True)
-            age_21_30 = st.checkbox("21-30", value=True)
-            age_31_40 = st.checkbox("31-40", value=True)
-            age_41_50 = st.checkbox("41-50", value=True)
-            age_51_60 = st.checkbox("51-60", value=True)
-            age_61_70 = st.checkbox("61-70", value=True)
-            age_71_80 = st.checkbox("71-80", value=True)
-            age_81_90 = st.checkbox("81-90", value=True)
-            age_91_100 = st.checkbox("91-100", value=True)
+            age_range = st.slider("Age Range", 0, 100, (0, 100), step=1)
 
         with st.expander("Select Insurance Plans"):
             insurance_plans_checkboxes = []
             for plan in insurance_options:
                 insurance_plans_checkboxes.append(st.checkbox(f"{plan}", value=True))
 
+
+if file_upload is not None:
     # Age group logic
     df["Age"] = df["Birth Date"].apply(calculate_age)
-    age_checkboxes = [age_0_10, age_11_20, age_21_30, age_31_40, age_41_50, age_51_60, age_61_70, age_71_80, age_81_90, age_91_100]
-    selected_ages = select_ages(age_checkboxes)
-    df = df[df["Age"].isin(selected_ages)]
+    df = df[df["Age"].isin(range(age_range[0], age_range[1] + 1))]
 
     # Insurance Group Logic
     insurance_plans_selected = [plan for plan, val in zip(insurance_options, insurance_plans_checkboxes) if val]
